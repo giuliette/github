446ceec57f4b9e17eafd6f3734b8f6d5f0adc6d7
EmeseEeva <emese.hackl@gmail.com>
2019-01-14 14:47:40 +0100

    C5

--- mesi.js
+++ mesi.js
@@ -1,7 +1,10 @@
 'use strict'
 
-function cheer(x) {
- console.log(x);
+let x = 'This is';
+let y = 'A TEAM'
+
+function cheer(x, y) {
+ console.log(x + y);
 }
 
-cheer('this is the TEAM');   
+cheer(x + '' + y);   
